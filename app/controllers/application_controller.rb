7b2558388909aa04fe415d6e8d061d797e2d96bd
militarpancho <militarpancho_93@msn.com>
2015-08-13 13:45:51 +0200

    Login and Internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,44 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_action :authorize
   protect_from_forgery with: :exception
+  before_action :set_i18n_locale_from_params
+
+  protected
+    
+    def set_i18n_locale_from_params 
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale]) 
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] =
+            "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end 
+      end
+    end
+
+
+    def default_url_options
+      { locale: I18n.locale }
+    end
+
+    def authorize
+        if request.format == Mime::HTML
+          unless User.find_by(id: session[:user_id])
+            if User.count.zero?
+              flash[:notice] = "Create an administrator account"
+              redirect_to :controller => 'admin', :action => 'login'
+            else  
+              redirect_to login_url, notice: "Please log in"
+            end  
+          end
+        else  
+          authenticate_or_request_with_http_basic do |username, password|
+             user = User.find_by_name(username)
+             user and user.authenticate(password)
+          end  
+        end
+
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,36 @@
 Rails.application.routes.draw do
-  resources :line_items
-  resources :carts
-  get 'store/index'
 
-  resources :products
+  get 'admin' => 'admin#index' 
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create 
+    delete 'logout' => :destroy
+  end
+
+  get "sessions/create" 
+  get "sessions/destroy"
+  
+  resources :users
+  
+  resources :products do
+    get :who_bought, on: :member
+  end  
+  
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end  
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'store#index', as: 'store'
+  
 
 
   
